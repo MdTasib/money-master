a5a185e635d42aaf145f9302843461f16279390f
MdTasib <ohidulalam442@gmail.com>
2022-02-17 09:56:31 +0600

    expenses count complete

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
+// get input value
+function getInputValue(input) {
+	const inputField = document.getElementById(input);
+	const inputAmountText = inputField.value;
+	const inputAmount = parseFloat(inputAmountText);
+
+	return inputAmount;
+
+	// inputField.value = "";
+}
+
+// total expenses function
+function getTotalExpense() {
+	// expenses
+	const foodExpenses = getInputValue("food");
+	const rentExpenses = getInputValue("rent");
+	const clotheExpenses = getInputValue("clothe");
+
+	const totalExpensesField = document.getElementById("total-expenses");
+	const totalExpensesText = totalExpensesField.innerText;
+	const previousTotalExpensesAmount = parseFloat(totalExpensesText);
+
+	// total expenses count
+	const totalExpensesCount =
+		previousTotalExpensesAmount + foodExpenses + rentExpenses + clotheExpenses;
+	console.log(totalExpensesCount);
+
+	// The value of total expenses is displayed
+	totalExpensesField.innerText = totalExpensesCount;
+}
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
 	const income = getInputValue("income");
 	console.log(income);
+
+	getTotalExpense();
 });
-
-document.getElementById("calculate-btn").addEventListener("click", function () {
-	getInputValue("food");
-});
-
-document.getElementById("calculate-btn").addEventListener("click", function () {
-	getInputValue("rent");
-});
-
-document.getElementById("calculate-btn").addEventListener("click", function () {
-	getInputValue("clothe");
-});
-
-function getInputValue(inputField) {
-	const incomeInput = document.getElementById(inputField);
-	const incomeAmountText = incomeInput.value;
-	const incomeAmount = parseFloat(incomeAmountText);
-
-	return incomeAmount;
-
-	// incomeInput.value = "";
-}

33916e122486bbbcdb568c06121a5275782342b8
MdTasib <ohidulalam442@gmail.com>
2022-02-17 09:35:09 +0600

    calculate btn handle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+document.getElementById("calculate-btn").addEventListener("click", function () {
+	const income = getInputValue("income");
+	console.log(income);
+});
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+	getInputValue("food");
+});
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+	getInputValue("rent");
+});
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+	getInputValue("clothe");
+});
+
+function getInputValue(inputField) {
+	const incomeInput = document.getElementById(inputField);
+	const incomeAmountText = incomeInput.value;
+	const incomeAmount = parseFloat(incomeAmountText);
+
+	return incomeAmount;
+
+	// incomeInput.value = "";
+}
